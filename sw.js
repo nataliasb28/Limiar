d36372f7b4126278dd6ccaa37f6bde1412b12200
nataliasb28 <naty_rsb@hotmail.com>
2026-01-26 15:04:06 -0300

    Add Câmara fragments for Analista Legislativo

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = "limiar-mvp11-v2";
+const CACHE_NAME = "limiar-v3";
 const ASSETS = ["./","./index.html","./manifest.json","./icon.svg","./sw.js"];
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache)=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-self.addEventListener("activate",(event)=>{ event.waitUntil(self.clients.claim()); });
-self.addEventListener("fetch",(event)=> {
-  event.respondWith(caches.match(event.request).then((cached)=> cached || fetch(event.request)));
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
 });
